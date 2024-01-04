cce77ab8fc8af1c0f56f0588922206f1ecf0625c
Pasindu Anjana <85106686+Pasindu420@users.noreply.github.com>
2024-01-04 12:45:33 +0530

    bug fixed

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "pasindux" #change this
+    bucket = "pasindux-tfstate-bkt" #change this
     key    = "terraform.tfstate"
     region = "us-east-1"
     #dynamodb_table = "my-lock-table" # optional (Only if you created the DynamoDB table in step 4) 
